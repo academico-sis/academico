1fe8cd94fbbcdefa1f64e3076f852048abd39b3c
Laxsmi <t@debay.org>
2018-12-17 08:34:29 -0500

    Sentry config

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Attendance;
 use App\Models\Period;
+use App\Exceptions\MissingBaseTables;
 
 class HomeController extends Controller
 {
