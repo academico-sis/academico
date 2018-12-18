a7f54a4bee421dbc39dfe928eba7248d23f2d7d9
Laxsmi <t@debay.org>
2018-12-17 20:38:19 -0500

    datatables

--- resources/views/courses/index.blade.php
+++ resources/views/courses/index.blade.php
@@ -38,9 +38,10 @@
             
             <div class="box-body">           
                 
-                <table id="crudTable" class="table table-striped table-bordered responsive">
+                <table id="crudTable" class="table table-striped responsive">
                     <thead>
                         <tr>
+                            <th></th>
                             <th>{{ trans_choice('academico.rythms', 1) }}</th>
                             <th>{{ trans_choice('academico.level', 1) }}</th>
                             <th>{{ trans_choice('academico.name', 1) }}</th>
@@ -58,6 +59,7 @@
                     <tbody>
                         @foreach ($courses as $course)
                         <tr>
+                        <td></td>
                             <td>{{ $course->course_rythm_name }}</td>
                             <td>{{ $course->course_level_name }}</td>
                             <td>{{ $course->name }}</td>
@@ -113,7 +115,7 @@
 @section('after_scripts')
  <!-- DATA TABLES -->
  <link href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap.min.css" rel="stylesheet" type="text/css" />
- <link href="https://cdn.datatables.net/responsive/2.2.3/css/responsive.dataTables.min.css">
+ <link href="https://cdn.datatables.net/responsive/2.2.3/css/responsive.dataTables.min.css" rel="stylesheet" type="text/css" />
 
  
  <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
@@ -121,8 +123,30 @@
  <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/responsive/2.2.3/js/dataTables.responsive.min.js"></script>
 
   <script>
-        $(document).ready( function () {
-            $('#crudTable').DataTable();
-        } );
+    $(document).ready( function () {
+        $('#crudTable').DataTable(
+            {
+                "scrollX": true,
+                paging: false,
+                responsive: {
+                details: {
+                    display: $.fn.dataTable.Responsive.display.childRow,
+                    type: 'column'
+                }
+            },
+            columnDefs: [
+                {
+                className: 'control',
+                orderable: false,
+                targets: 0
+                },
+                { responsivePriority: 5, targets: 3 },
+                { responsivePriority: 6, targets: 5 },
+                { responsivePriority: 11000, targets: 4 },
+                { responsivePriority: 6, targets: -1 }
+            ]
+            }
+        );
+    } );
 </script>
 @endsection
