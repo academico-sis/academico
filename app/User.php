885cf11774efd3950949ee36c2a8f49debcea939
Laxsmi <t@debay.org>
2018-12-17 20:56:05 -0500

    enrollment test

--- app/User.php
+++ app/User.php
@@ -32,6 +32,17 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
+    public function enroll(\App\Models\Course $course)
+    {
+        $enrollment = new \App\Models\Enrollment;
+        $enrollment->user_id = $this->id;
+        $enrollment->responsible_id = 1;
+        $enrollment->course_id = $course->id;
+        $enrollment->status = 1;
+
+        $enrollment->save();
+    }
+
     public function getNameAttribute()
     {
         return $this->firstname . ' ' . $this->lastname;
---
+++ tests/Feature/EnrollmentTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class EnrollmentTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_that_a_new_enrollment_appears_in_student_list()
+    {
+        // given a user
+        $student = \App\User::find(1);
+
+        // and a course
+        $course = \App\Models\Course::find(1);
+        
+        // if we enroll the user in the course
+        $student->enroll($course);
+
+        // they appear on the student roaster
+        $user = \App\User::find(2);
+        \Auth::guard(backpack_guard_name())->login($user);
+
+        $response = $this->get("/course/$course->id");
+        
+        // with their name
+        $response->assertSee("$student->name");
+
+        // todo and their age
+    }
+}
