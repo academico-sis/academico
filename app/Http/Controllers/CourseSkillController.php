0889a04f6bcd96c6481960879e81d9c0714a70d3
Laxsmi <t@debay.org>
2019-01-13 13:47:32 -0500

    skills course attach

--- app/Http/Controllers/CourseSkillController.php
+++ app/Http/Controllers/CourseSkillController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Course;
 use App\Models\Skill;
-
+use App\Models\Course;
 use Illuminate\Http\Request;
+use Prologue\Alerts\Facades\Alert;
 
 class CourseSkillController extends Controller
 {
@@ -19,11 +19,32 @@ class CourseSkillController extends Controller
      */
     public function edit(Course $course)
     {
-        $all_skills = Skill::all()->groupBy(['level_id', 'skill_type_id']);
-
         $course_skills = $course->skills;
+        //return $course_skills;
+        // filter here (by level)
+        $all_skills = Skill::with('skill_type')->with('level')->get();
+        $skills = $all_skills->map(function ($item, $key) use($course_skills) {
+            $item['attached'] = false;
+            if ($course_skills->has($item->id)) {
+                $item['attached'] = true;
+            }
+            return $item;
+        });
+        //dd($skills);
+        return view('skills.edit', compact('course', 'skills'));
+    }
 
-        return view('skills.edit', compact('course', 'all_skills', 'course_skills'));
+    public function update(Course $course, Request $request)
+    {
+        $course->skills()->detach();
+        foreach ($request->input('skill') as $key => $skill)
+        {
+            $course->skills()->attach(Skill::find($key),
+            ['weight' => 1]);
+        }
+        Alert::success('Skills set was saved for the course')->flash();
+
+        return back();
     }
 
 }
--- resources/views/skills/edit.blade.php
+++ resources/views/skills/edit.blade.php
@@ -13,33 +13,47 @@
 
 <div class="row">
     <div class="col-md-12">
+            <form action="/course/{{$course->id}}/skills" method="post">
+                @method('patch')
+                @csrf
         <div class="box">
             <div class="box-header with-border">
                 <div class="box-title">
-                    @lang('grades_edit')
+                    @lang('Edit Course Skills')
                 </div>
-                
+
                 <div class="box-tools pull-right">
-                    <a class="btn btn-primary" href=""><i class="fa fa-plus"></i>Add Skill</a>
+                    <button class="btn btn-success pull-right" type="submit">
+                        @lang('Save skills for course')
+                    </button>
                 </div>  
             </div>
             
             <div class="box-body">           
-               <table class="table">
+               <table class="table table-striped" id="skillsTable">
                    <thead>
+                       <td></td>
                        <td>Skill Type</td>
                        <td>Level</td>
                        <td>Skill</td>
                    </thead>
-                   
-                   @foreach ($all_skills as $skill)
-                   <tr>
-                       <td></td>
-                   </tr>
-                   @endforeach
+                   <tbody>
+                       @foreach ($skills as $skill)
+                            <tr>
+                                <td><input type="checkbox" name="skill[{{$skill->id}}]" id="skill[{{$skill->id}}]" @if($skill->attached == '1') checked @endif></td>
+                                <td>{{ $skill->level->name }}</td>
+                                <td>{{ $skill->skill_type->shortname }}</td>
+                                <td>
+                                    <label for="skill[{{$skill->id}}]">{{ $skill->name }}</label>
+                                </td>
+                                <td></td>
+                            </tr>
+                        @endforeach
+                   </tbody>
                </table>
             </div>
         </div>
+    </form>
     </div>
 </div>
 
