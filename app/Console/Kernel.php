0ca8be35a06c4ee9f2730fad9e84bbc150eed06d
Laxsmi <t@debay.org>
2019-02-09 15:01:33 -0500

    attendance reminders

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,7 +28,7 @@ class Kernel extends ConsoleKernel
     {
         $schedule->call(function () {
             (new Attendance)->remindPendingAttendance();
-        })->everyMinute();
+        })->daily();
 
     }
 
