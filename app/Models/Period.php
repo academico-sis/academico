a3399685937b2603724b568efeb913c9335bbcd7
Laxsmi <t@debay.org>
2019-02-13 11:12:14 -0500

    temp period selection force

--- app/Models/Period.php
+++ app/Models/Period.php
@@ -20,6 +20,7 @@ class Period extends Model
   
     public static function get_default_period()
     {
+        return Period::find(22);
         return Period::where('start', '<=', date('Y-m-d'))
         ->where('end', '>=', date('Y-m-d'))
         ->first();
