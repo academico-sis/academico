1e37565a80be08aafc6fec9759cb4ce09b0c1d7c
Laxsmi <t@debay.org>
2019-02-19 12:13:07 -0500

    ui

--- resources/views/courses/show.blade.php
+++ resources/views/courses/show.blade.php
@@ -118,7 +118,7 @@
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">X</span></button>
           <h4 class="modal-title" id="myModalLabel">@lang('Enroll new student')</h4>
         </div>
   
