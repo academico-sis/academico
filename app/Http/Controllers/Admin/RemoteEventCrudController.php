3d61bb2c0490198b0350c15ce6416b3b69bc77da
Laxsmi <t@debay.org>
2019-02-20 08:54:19 -0500

    fix

--- app/Http/Controllers/Admin/RemoteEventCrudController.php
+++ app/Http/Controllers/Admin/RemoteEventCrudController.php
@@ -83,7 +83,7 @@ class RemoteEventCrudController extends CrudController
                 'label' => "Teacher", // Table column heading
                 'type' => "select",
                 'name' => 'teacher_id', // the column that contains the ID of that connected entity;
-                'entity' => 'user', // the method that defines the relationship in your Model
+                'entity' => 'teacher', // the method that defines the relationship in your Model
                 'attribute' => "name", // foreign key attribute that is shown to user
                 'model' => "App\Models\Teacher", // foreign key model
              ],
