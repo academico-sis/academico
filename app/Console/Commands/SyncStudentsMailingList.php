74a3bbe157968c8cf624a25654d7296963709749
Laxsmi <t@debay.org>
2019-02-21 08:49:21 -0500

    cleaning

--- app/Console/Commands/SyncStudentsMailingList.php
+++ app/Console/Commands/SyncStudentsMailingList.php
@@ -6,9 +6,28 @@ use App\Models\Period;
 use Illuminate\Http\Request;
 use Illuminate\Console\Command;
 
-class SyncStudentsMailingList extends Command
-{
+/** wip - todo - the interface we use */
+interface Mailer {
+    /**
+     * createSubscriber
+     *
+     * @param $student object with email, firstname, lastname
+     * @param int $group
+     * @return void
+     */
+    public function createSubscriber($student, $group);
+    
+    /**
+     * unsubscribeSubscriber
+     *
+     * @param string $email
+     * @return void
+     */
+    public function unsubscribeSubscriber($email);
+}
 
+class SyncStudentsMailingList extends Command implements Mailer
+{
 
     /**
      * The name and signature of the console command.
@@ -22,7 +41,7 @@ class SyncStudentsMailingList extends Command
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Sync users with an external mailing service';
 
     /**
      * Create a new command instance.
@@ -35,76 +54,6 @@ class SyncStudentsMailingList extends Command
         
         $this->api_key = env('mailerlite_api_key');
     }
-
-    private function add_users_to_group($source_users, $target_group_id)
-    {
-        $groupsApi = (new \MailerLiteApi\MailerLite($this->api_key))->groups();
-        $subscribersApi = (new \MailerLiteApi\MailerLite($this->api_key))->subscribers();
-
-        $target_users = $groupsApi->limit(10000)->getSubscribers($target_group_id);
-
-        $all_subscribers = $subscribersApi->limit(10000)->get(); // todo watch this limit
-
-        // step 1 - add source users to the target group
-        foreach ($source_users as $student)
-        {
-            $match = false;
-            $exist = false;
-
-            unset($created_subscriber);
-
-            foreach ($target_users as $subscriber)
-            {
-                if (strtolower($student->email) == strtolower($subscriber->email))
-                {
-                    $match = true;
-                    break 1;
-                }
-            }
-            if ($match == false) {
-                // check if the student exists in ML
-                foreach ($all_subscribers as $subscriber)
-                {
-                    if (strtolower($student->email) == strtolower($subscriber->email))
-                    {
-                        $exist = true;
-                        break 1;
-                    }
-                }
-
-                // if they do not exist, create them
-                if ($exist == false)
-                {
-
-                    dd($student->email);
-                    $new_subscriber = [
-                        'email' => $student->email,
-                        'name' => ucwords($student->firstname),
-                        'fields' => [
-                            'last_name' => ucwords($student->lastname),
-                            'birthdate' => $student->birthdate ?? ''
-                        ]
-                        ];
-                
-                    $created_subscriber = $subscribersApi->create($new_subscriber);
-                    var_dump($created_subscriber);
-                }
-
-                if(!isset($created_subscriber)) {
-                    $created_subscriber = $subscribersApi->find($student->email);
-                }
-
-                // add them to the group.
-                $addedSubscriber = $groupsApi->addSubscriber($target_group_id, $created_subscriber);
-                var_dump($addedSubscriber);
-            }
-            sleep(1);
-
-
-        }
-    }
-
-
 
 
 
@@ -208,14 +157,12 @@ class SyncStudentsMailingList extends Command
         {
             if ($enrollment->student->lead_type_id == 1) // converted
             {
-
                 dump($this->createStudentSubscriber($enrollment->student));
 
                 foreach ($enrollment->student->contacts as $contact)
                 {
                     dump($this->createParentSubscriber($contact));
                 }
-
             }
 
             if($enrollment->student->lead_type_id == 3) // dead
@@ -231,21 +178,14 @@ class SyncStudentsMailingList extends Command
 
             if($enrollment->student->lead_type_id == 6) // exAlumno
             {
-                // unsubscribe
-                $this->unsubscribeSubscriber($enrollment->student->email);
+                $this->createAlumniSubscriber($enrollment->student);
 
                 foreach ($enrollment->student->contacts as $contact)
                 {
-                    dump($this->unsubscribeSubscriber($contact->email));
+                    dump($this->createAlumniSubscriber($contact));
                 }
             }
         }
 
-
-        // todo clean ETUDIANTS ACTUELS group - remove student whose status have changed.
-        // but do not remove them right away, maybe wait a little
-
-
-        
     }
 }
