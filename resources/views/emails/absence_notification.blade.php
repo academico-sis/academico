69bd0d1e33df191311001c401328580ebc4b639b
Laxsmi <t@debay.org>
2019-04-06 11:49:26 -0500

    show course name in absence notification

--- resources/views/emails/absence_notification.blade.php
+++ resources/views/emails/absence_notification.blade.php
@@ -1,6 +1,6 @@
 <h3>@lang('Hi')</h3>
 
-<p>@lang('This is an absence notification for') {{ $student->name }} @lang('on') {{ $event->start }}</p>
+<p>@lang('This is an absence notification for') {{ $student->name }} @lang('on') {{ $event->start }} ({{$event->name}}).</p>
 
 <p>@lang('Thank you for justificating this absence by replying to this email').</p>
 
