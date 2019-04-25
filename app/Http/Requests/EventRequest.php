9aae1d507a3d9219c9397a5a36b4bb2313604524
Laxsmi <t@debay.org>
2019-04-25 10:02:18 -0500

    validate events input

--- app/Http/Requests/EventRequest.php
+++ app/Http/Requests/EventRequest.php
@@ -26,7 +26,9 @@ class EventRequest extends FormRequest
     public function rules()
     {
         return [
-            // 'name' => 'required|min:5|max:255'
+            'name' => 'required|min:3|max:255',
+            'start' => 'required|date',
+            'end' => 'required|date',
         ];
     }
 
