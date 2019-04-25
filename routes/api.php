e4f1734360575da11d3a5bc02685361618f20b47
Laxsmi <t@debay.org>
2019-04-25 13:35:21 -0500

    basic api token auth

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,6 @@ Route::middleware('auth:api')->get('/attendance', function () {
     return Teacher::where('user_id', request()->user()->id)->firstOrFail()->events_with_pending_attendance;
 });
  
-//
 Route::middleware('auth:api')->get('/teacherinfo', function () {
     return Teacher::where('user_id', request()->user()->id)->firstOrFail();
 });
