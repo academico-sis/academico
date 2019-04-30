d1cb3d0119303249cb4a23a207025a57c1ca88f0
Laxsmi <t@debay.org>
2019-04-30 15:31:53 -0500

    update middleware

--- app/Http/Middleware/ForceUpdate.php
+++ app/Http/Middleware/ForceUpdate.php
@@ -15,30 +15,30 @@ class ForceUpdate
      */
     public function handle($request, Closure $next)
     {
+        
 if (backpack_user() != null) {
 
-
-        if ((!\Request::is('update')) && backpack_user()->isStudent() && backpack_user()->student->force_update == 1) {
+        if (backpack_user()->isStudent() && backpack_user()->student->force_update == 1) {
             return redirect('/update');
         }
 
-        if ((!\Request::is('update/2')) && backpack_user()->isStudent() && backpack_user()->student->force_update == 2) {
+        if (backpack_user()->isStudent() && backpack_user()->student->force_update == 2) {
             return redirect('/update/2');
         }
 
-         if ((!\Request::is('update/3') || !\Request::is('update/3')) && backpack_user()->isStudent() && backpack_user()->student->force_update == 3) {
+         if (backpack_user()->isStudent() && backpack_user()->student->force_update == 3) {
             return redirect('/update/3');
         }
 
-        if ((!\Request::is('update/4')) && backpack_user()->isStudent() && backpack_user()->student->force_update == 4) {
+        if (backpack_user()->isStudent() && backpack_user()->student->force_update == 4) {
             return redirect('/update/4');
         }
 
-        if ((!\Request::is('update/5')) && backpack_user()->isStudent() && backpack_user()->student->force_update == 5) {
+        if (backpack_user()->isStudent() && backpack_user()->student->force_update == 5) {
             return redirect('/update/5');
         }
 
-         if ((!\Request::is('update/6')) && backpack_user()->isStudent() && backpack_user()->student->force_update == 6) {
+         if (backpack_user()->isStudent() && backpack_user()->student->force_update == 6) {
             return redirect('/update/6');
         }
 
