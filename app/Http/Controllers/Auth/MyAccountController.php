bc471ade575c48e822dcdf2842efd8b383c6e1da
Thomas <t@debay.org>
2019-05-12 15:51:48 -0500

    wip data update steps 1 and 2

--- app/Http/Controllers/Auth/MyAccountController.php
+++ app/Http/Controllers/Auth/MyAccountController.php
@@ -29,6 +29,7 @@ class MyAccountController extends Controller
         return view('backpack::auth.account.update_info', $this->data);
     }
 
+
     /**
      * Save the modified personal information for a user.
      */
@@ -44,6 +45,36 @@ class MyAccountController extends Controller
 
         return redirect()->back();
     }
+
+
+    /**
+     * Show the student a form to change his personal information.
+     * The difference with getAccountInfoForm is that the former is available to all users. This one is specific to stuents (different DB tables)
+     */
+    public function getStudentInfoForm()
+    {
+        $this->data['title'] = trans('backpack::base.my_account');
+        $this->data['user'] = $this->guard()->user()->student;
+
+        return view('backpack::auth.account.update_student_info', $this->data);
+    }
+
+    /**
+     * Save the modified personal information for a user.
+     */
+    public function postStudentInfoForm(Request $request)
+    {
+        $result = $this->guard()->user()->student()->update($request->except(['_token']));
+
+        if ($result) {
+            Alert::success(trans('backpack::base.account_updated'))->flash();
+        } else {
+            Alert::error(trans('backpack::base.error_saving'))->flash();
+        }
+
+        return redirect()->back();
+    }
+
     /**
      * Get the guard to be used for account manipulation.
      *
---
+++ tests/Feature/UpdateDataTest.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\Student;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UpdateDataTest extends TestCase
+{
+
+    use RefreshDatabase;
+
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->seed('DatabaseSeeder');
+        $this->student = factory(Student::class)->create();
+    }
+
+    /**
+     * @test
+     * 
+     * When a user has a force_update DB record set, they are redirected to the update screen corresponding to the specified update step
+     */
+    public function SelectedUsersAreRedirectedToRelevantUpdateScreen()
+    {
+
+    }
+
+
+    /**
+     * Users may view the edit screen for basic data
+     * This is Update Step 1
+     * @test
+     */
+    public function UsersMayUpdateBasicData()
+    {
+        \Auth::guard(backpack_guard_name())->login($this->student->user);
+        $response = $this->get(route('backpack.account.info')); // user data edition
+        $response->assertStatus(200);
+        $response->assertSee($this->student->email);
+        $response->assertSee($this->student->firstname);
+        $response->assertSee($this->student->lastname);
+    }
+
+    // todo duplicate method ( edit // update) and test data saving to DB
+
+
+    /**
+     * Users may view the edit screen for additional data
+     * This is Update Step 2
+     * @test
+     */
+    public function UsersMayUpdateAdditionalData()
+    {
+        \Auth::guard(backpack_guard_name())->login($this->student->user);
+        $response = $this->get(route('backpack.student.info')); // student data edition
+        $response->assertStatus(200);
+        $response->assertSee($this->student->birthdate);
+        $response->assertSee($this->student->address);
+        $response->assertSee($this->student->idnumber);
+    }
+
+    /**
+     * Users may view the edit screen for phone numbers
+     * This is Update Step 3
+     * @test
+     */
+    public function UsersMayUpdatePhoneNumbers()
+    {
+        \Auth::guard(backpack_guard_name())->login($this->student->user);
+        $response = $this->get(route('backpack.account.info'));
+        $response->assertStatus(200);
+        $response->assertSee();
+    }
+
+    /**
+     * Users may update their profile picture
+     * This is Update Step 4
+     * @test
+     */
+    public function UsersMayUpdateProfilePicture()
+    {
+        \Auth::guard(backpack_guard_name())->login($this->student->user);
+        $response = $this->get(route('backpack.account.info'));
+        $response->assertStatus(200);
+        $response->assertSee();
+    }
+
+    /**
+     * Users may view the edit screen for profession and institution
+     * This is Update Step 5
+     * @test
+     */
+    public function UsersMayUpdateWorkData()
+    {
+        \Auth::guard(backpack_guard_name())->login($this->student->user);
+        $response = $this->get(route('backpack.account.info'));
+        $response->assertStatus(200);
+        $response->assertSee();
+    }
+
+    /**
+     * Users may view the edit screen for additional contacts
+     * This is Update Step 6
+     * @test
+     */
+    public function UsersMayUpdateAdditionalContacts()
+    {
+        \Auth::guard(backpack_guard_name())->login($this->student->user);
+        $response = $this->get(route('backpack.account.info'));
+        $response->assertStatus(200);
+        $response->assertSee();
+    }
+}
