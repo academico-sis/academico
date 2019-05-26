5f2f85b8fc0ab42578338244d8db7507a1be0b83
Thomas <t@debay.org>
2019-05-26 14:33:14 -0500

    versioning

--- resources/views/vendor/backpack/base/inc/footer.blade.php
+++ resources/views/vendor/backpack/base/inc/footer.blade.php
@@ -6,3 +6,5 @@
     {{ trans('backpack::base.handcrafted_by') }} <a target="_blank" href="{{ config('backpack.base.developer_link') }}">{{ config('backpack.base.developer_name') }}</a>.
     @endif
   @endif
+
+  <div class="pull-right hidden-xs">@version('compact')</div>
