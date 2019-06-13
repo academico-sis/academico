84a57aa5d38ba7fe7dba345dbac4626171f74f5a
Thomas <t@debay.org>
2019-06-13 22:29:52 +0000

    backpack datetime format

--- config/backpack/base.php
+++ config/backpack/base.php
@@ -31,10 +31,9 @@ return [
     'skin' => env('APP_COLOR', 'skin-red'),
     // Options: skin-black, skin-blue, skin-purple, skin-red, skin-yellow, skin-green, skin-blue-light, skin-black-light, skin-purple-light, skin-green-light, skin-red-light, skin-yellow-light
 
-    // Date & Datetime Format Syntax: https://github.com/jenssegers/date#usage
-    // (same as Carbon)
-    'default_date_format'     => 'j F Y',
-    'default_datetime_format' => 'j F Y H:i',
+    // Date & Datetime Format Syntax: https://carbon.nesbot.com/docs/#api-localization
+    'default_date_format'     => 'D MMM YYYY',
+    'default_datetime_format' => 'D MMM YYYY, HH:mm',
 
     // Content of the HTML meta robots tag to prevent indexing and link following
     'meta_robots_content' => 'noindex, nofollow',
