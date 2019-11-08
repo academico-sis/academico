37155850dd38518bf23baa657bc705805af3fcf5
Thomas <thomas@debay.org>
2019-11-08 17:49:02 +0800

    add language switcher links

--- resources/views/vendor/backpack/base/inc/topbar_right_content.blade.php
+++ resources/views/vendor/backpack/base/inc/topbar_right_content.blade.php
@@ -4,3 +4,9 @@
 {{-- <li class="nav-item d-md-down-none"><a class="nav-link" href="#"><i class="fa fa-bell"></i><span class="badge badge-pill badge-danger">5</span></a></li>
 <li class="nav-item d-md-down-none"><a class="nav-link" href="#"><i class="fa fa-list"></i></a></li>
 <li class="nav-item d-md-down-none"><a class="nav-link" href="#"><i class="fa fa-map"></i></a></li> --}}
+
+@foreach (language()->allowed() as $code => $name)
+<li class="nav-item px-3">
+    <a class="nav-link" href="{{ language()->back($code) }}">{{ $name }}</a>
+</li>
+@endforeach
