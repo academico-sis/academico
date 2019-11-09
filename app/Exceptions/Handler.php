19b8f52add77ef2e16392d6400594a216531c2e6
Thomas <thomas@debay.org>
2019-11-09 15:27:08 +0800

    Auto stash before merge of "backpack4" and "master"

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -18,6 +18,23 @@ class Handler extends ExceptionHandler
     ];
 
     /**
+     * A list of the internal exception types that should not be reported.
+     * (overwrite vendor file)
+     *
+     * @var array
+     */
+     protected $internalDontReport = [
+        AuthenticationException::class,
+        //AuthorizationException::class,
+        //HttpException::class,
+        //HttpResponseException::class,
+        //ModelNotFoundException::class,
+        SuspiciousOperationException::class,
+        TokenMismatchException::class,
+        //ValidationException::class,
+    ];
+
+    /**
      * A list of the inputs that are never flashed for validation exceptions.
      *
      * @var array
