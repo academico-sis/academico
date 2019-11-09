e62bf9e6898ba96ca78b3ac2480b7ecb9427d07a
Thomas <thomas@debay.org>
2019-11-09 10:53:55 +0800

    clean js dependencies

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,5 +12,4 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css')
    .copy('node_modules/@fullcalendar', 'public/fullcalendar');
