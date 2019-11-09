bd46e13d7152a7453dac02573a68b7259889ae96
Thomas <thomas@debay.org>
2019-11-09 15:34:36 +0800

    include sentry in error layouts

--- resources/views/errors/layout.blade.php
+++ resources/views/errors/layout.blade.php
@@ -8,7 +8,7 @@
 
         <!-- Fonts -->
         <link rel="dns-prefetch" href="//fonts.gstatic.com">
-        <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
 
         <!-- Styles -->
         <style>
@@ -35,27 +35,22 @@
                 position: relative;
             }
 
-            .code {
-                border-right: 2px solid;
-                font-size: 26px;
-                padding: 0 15px 0 15px;
+            .content {
                 text-align: center;
             }
 
-            .message {
-                font-size: 18px;
-                text-align: center;
+            .title {
+                font-size: 36px;
+                padding: 20px;
             }
         </style>
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            <div class="code">
-                @yield('code')
-            </div>
-
-            <div class="message" style="padding: 10px;">
-                @yield('message')
+            <div class="content">
+                <div class="title">
+                    @yield('message')
+                </div>
             </div>
         </div>
 
@@ -69,5 +64,6 @@
                 });
             </script>
         @endif
+
     </body>
 </html>
