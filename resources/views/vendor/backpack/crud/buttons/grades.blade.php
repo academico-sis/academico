ab096bbef9a2f8d0bf2b823aa4dc3a2de8eb0fb9
Thomas <thomas@debay.org>
2019-11-11 09:15:47 +0800

    icon

--- resources/views/vendor/backpack/crud/buttons/grades.blade.php
+++ resources/views/vendor/backpack/crud/buttons/grades.blade.php
@@ -1,5 +1,5 @@
 @if($entry->evaluation_type->contains(1))
 	<a href="{{ url('/course/'.$entry->getKey()) }}/grades" class="btn btn-sm btn-link">
-		<i class="fa fa-percent"></i>
+		<i class="fa fa-bar-chart"></i>
 	</a>
 @endif
