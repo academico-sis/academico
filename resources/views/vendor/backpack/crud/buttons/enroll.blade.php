5e061d1ff5c69c064367ecb98f902ceff92f71fa
Thomas <thomas@debay.org>
2019-11-21 10:17:48 +0800

    BUGFIX enrolments

--- resources/views/vendor/backpack/crud/buttons/enroll.blade.php
+++ resources/views/vendor/backpack/crud/buttons/enroll.blade.php
@@ -1,7 +1,7 @@
 @if($entry->opened == 1 && $entry->course_enrollments_count < $entry->spots)
 
 <form method="POST" action="{{ route('storeEnrollment') }}">
-    <input type="hidden" name="student_id" value="{{ $crud->student->user->id }}">
+    <input type="hidden" name="student_id" value="{{ $crud->student->id }}">
     <input type="hidden" name="course_id" value="{{ $entry->getKey() }}">
     @csrf
     <button type="submit" class="btn btn-sm btn-primary btn-link"><i class="fa fa-user-plus"></i></button>
