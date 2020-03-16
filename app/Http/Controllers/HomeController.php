62596fd54e1ec02f8a8b5332e7e6d184b600b8f3
Thomas Debay <thomas@debay.org>
2020-03-16 15:22:44 +0100

    optimize events array for dashboard calendar

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -101,9 +101,9 @@ class HomeController extends Controller
         Log::info(backpack_user()->firstname . ' ' . backpack_user()->lastname . " accessed the admin dashboard");
 
 
-        // todo refactor this !!
-        $events = Event::orderBy('id', 'desc')->get()->toArray();
-        
+        // todo optimize this !!
+        $events = Event::where('start', '>', (Carbon::now()->subDays(15)))->where('end', '<', (Carbon::now()->addDays(15)))->orderBy('id', 'desc')->get()->toArray();
+
         $teachers = Teacher::with('user')->get()->toArray();
 
         $teachers = array_map(function($teacher) {
@@ -129,7 +129,7 @@ class HomeController extends Controller
             'currentPeriod' => $currentPeriod,
             'enrollmentsPeriod' => $enrollmentsPeriod,
             'total_enrollment_count' => $currentPeriod->internal_enrollments_count,
-            'pending_attendance' => count((new Attendance)->get_pending_attendance()),
+            'pending_attendance' => count((new Attendance)->get_pending_attendance()),  // optimize
             'unassigned_events' => (new Event)->unassigned_teacher->count(),
             'upcoming_leaves' => Leave::upcoming_leaves(),
             'resources' => $teachers,
