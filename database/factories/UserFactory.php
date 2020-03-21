eadf64687573bbe6e5b856a4d649451e94d62ebd
Laravel Shift <shift@laravelshift.com>
2020-03-21 14:09:41 +0000

    Shift deprecated helpers
    Laravel recommends using the `Str` and `Arr` class methods directly instead of the respective helper functions. These helper functions are [deprecated in Laravel 5.8][1] and will be removed in a future version.
    
    [1]: https://github.com/laravel/framework/pull/26898

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Str;
 use Carbon\Carbon;
 use Faker\Generator as Faker;
 
@@ -21,7 +22,7 @@ $factory->define(App\Models\User::class, function (Faker $faker) {
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-        'remember_token' => str_random(10),
+        'remember_token' => Str::random(10),
         'locale' => 'fr',
     ];
 });
