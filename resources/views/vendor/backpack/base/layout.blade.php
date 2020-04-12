36c888efa7cb56df15af12dd4261c32fe88b9a79
Thomas Debay <thomas@debay.org>
2020-04-12 11:02:54 +0200

    add favicons

--- resources/views/vendor/backpack/base/layout.blade.php
+++ resources/views/vendor/backpack/base/layout.blade.php
@@ -2,6 +2,9 @@
 <html lang="{{ app()->getLocale() }}">
 <head>
     @include('backpack::inc.head')
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
 </head>
 <body class="hold-transition {{ config('backpack.base.skin') }} sidebar-mini">
 	<script type="text/javascript">
