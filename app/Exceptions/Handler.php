3614ff8bdde779bc77d9ba81535a93690d2753fc
Thomas Debay <thomas@debay.org>
2020-04-22 08:36:24 +0200

    use default dontreport

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -9,32 +9,6 @@ use Illuminate\Support\Facades\Log;
 class Handler extends ExceptionHandler
 {
     /**
-     * A list of the exception types that are not reported.
-     *
-     * @var array
-     */
-    protected $dontReport = [
-        //
-    ];
-
-    /**
-     * A list of the internal exception types that should not be reported.
-     * (overwrite vendor file).
-     *
-     * @var array
-     */
-    protected $internalDontReport = [
-        AuthenticationException::class,
-        AuthorizationException::class,
-        HttpException::class,
-        HttpResponseException::class,
-        //ModelNotFoundException::class,
-        SuspiciousOperationException::class,
-        TokenMismatchException::class,
-        ValidationException::class,
-    ];
-
-    /**
      * A list of the inputs that are never flashed for validation exceptions.
      *
      * @var array
