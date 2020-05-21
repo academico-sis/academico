ba858fc02f2648c8879dacba152471488924678f
Thomas Debay <thomas@debay.org>
2020-05-21 20:42:42 +0200

    Setup Slack log driver

--- config/logging.php
+++ config/logging.php
@@ -17,7 +17,7 @@ return [
     |
     */
 
-    'default' => env('LOG_CHANNEL', 'stack'),
+    'default' => env('LOG_CHANNEL', ['stack', 'slack']),
 
     /*
     |--------------------------------------------------------------------------
@@ -57,9 +57,8 @@ return [
         'slack' => [
             'driver' => 'slack',
             'url' => env('LOG_SLACK_WEBHOOK_URL'),
-            'username' => 'Laravel Log',
-            'emoji' => ':boom:',
-            'level' => 'critical',
+            'username' => 'Academico Log',
+            'level' => 'info',
         ],
 
         'papertrail' => [
