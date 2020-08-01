4d99c064bebd7c9605bc332467b6a3e9edbaa051
Thomas Debay <thomas@debay.org>
2020-08-01 18:23:49 +0200

    fix typo

--- app/Models/Period.php
+++ app/Models/Period.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 use Backpack\CRUD\app\Models\Traits\CrudTrait;
 use Carbon\Carbon;
-use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
 use Spatie\Activitylog\Traits\LogsActivity;
