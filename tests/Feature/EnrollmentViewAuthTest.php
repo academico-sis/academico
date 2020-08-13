0a459d54a5682b850c041f3caf171ddf74daa773
AleksandarMitevski <59975218+AleksandarMitevski@users.noreply.github.com>
2020-08-13 08:50:28 +0200

    Changes to test classes (#208)

---
+++ tests/Feature/EnrollmentViewAuthTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use App\Models\Enrollment;
+use App\Models\Student;
+use App\Models\Teacher;
+use App\Models\Course;
+use App\Models\User;
+use Tests\TestCase;
+
+class EnrollmentViewAuthTest extends TestCase
+{
+
+    use RefreshDatabase;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->seed('DatabaseSeeder');
+    }
+
+    /** @test **/
+    public function unathorized_users_cannot_view_enrollments()
+    {
+        $response = $this->get('enrollment');
+        $response -> assertStatus(302);
+        $response -> assertRedirect('/login');
+    }
+
+    /** @test **/
+    public function teachers_cannot_view_enrollments()
+    {
+        $teacher = factory(Teacher::class)->create();
+        backpack_auth()->login($teacher->user, true);
+    
+        $response = $this->get('enrollment');
+        $response -> assertStatus(403);
+
+    }
+    /** @test **/
+    public function students_cannot_view_enrollments()
+    {
+        $student = factory(Student::class)->create();
+        backpack_auth()->login($student->user,true);
+
+        $response = $this->get('enrollment');
+        $response -> assertStatus(403);
+    }
+
+    /** @test **/
+    public function admins_may_view_enrollments()
+    {
+        $user = factory(User::class)->create();
+        $user->assignRole('admin');
+
+        \Auth::guard(backpack_guard_name())->login($user);
+
+        $response = $this->get('enrollment');
+        $response->assertSee('Enrollments');
+    }
+
+    /** @test **/
+    public function secreteries_may_view_enrollments()
+    {
+        $user = factory(User::class)->create();
+        $user->assignRole('secretary');
+
+        \Auth::guard(backpack_guard_name())->login($user);
+
+        $response = $this->get('enrollment');
+        $response->assertSee('Enrollments');
+    }
+}
