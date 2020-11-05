0bab942f6c6fcf0c597772e39fbfb151b62756b4
Thomas Debay <thomas@debay.org>
2020-11-05 11:37:22 +0100

    Fix contact policy

--- app/Models/Policies/ContactPolicy.php
+++ app/Models/Policies/ContactPolicy.php
@@ -11,21 +11,21 @@ class ContactPolicy
     use HandlesAuthorization;
 
     /**
-     * Create a new policy instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
-
-    /**
      * the user may update this information if they are the student whom the contact is related to,
      * or if they have explicit permission to update any contact (students).
      */
     public function update(User $user, Contact $contact)
     {
-        return $user->student->id ?? null == $contact->student_id || $user->can('student.edit');
+        if ($user->can('student.edit'))
+        {
+            return true;
+        }
+
+        if ($user->student) {
+            return $user->student->id == $contact->student_id;
+        }
+
+        return false;
     }
+}
 }
