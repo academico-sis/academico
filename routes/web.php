5026ca07cc3eb28129e20e8bcac07d74c24431bc
Thomas Debay <thomas@debay.org>
2021-02-07 11:31:46 +0100

    clean test files

--- routes/web.php
+++ routes/web.php
@@ -224,9 +224,3 @@ Route::group([
     Route::put('/student/{student}', 'StudentController@update')->name('student.update');
     Route::post('/student', 'StudentController@store')->name('student.store');
 });
-
-Route::group([
-    'middleware' => ['web', 'role:admin', 'language'],
-], function () {
-    Route::get('launchapi', 'LMSController@createUser');
-});
