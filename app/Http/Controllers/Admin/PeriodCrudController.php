97a57beab53e1859d2eba78036bea9557a47fb4b
Thomas Debay <thomas@debay.org>
2021-02-17 12:04:30 +0100

    fix Period CRUD

--- app/Http/Controllers/Admin/PeriodCrudController.php
+++ app/Http/Controllers/Admin/PeriodCrudController.php
@@ -62,8 +62,7 @@ class PeriodCrudController extends CrudController
         CRUD::addFields([
             [
                 'type' => "relationship",
-                'name' => 'year', // the method on your model that defines the relationship
-                'ajax' => false,
+                'name' => 'year_id', // the method on your model that defines the relationship
                 'inline_create' => true, // assumes the URL will be "/admin/category/inline/create"
             ],
 
