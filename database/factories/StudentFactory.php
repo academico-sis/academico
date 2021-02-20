b6bca168cc3a953cb2a8c68e3ba06b5ae89947ca
Thomas Debay <thomas@debay.org>
2021-02-20 08:38:37 +0100

    Fix broken seeder

--- database/factories/StudentFactory.php
+++ database/factories/StudentFactory.php
@@ -8,7 +8,6 @@ use Faker\Generator as Faker;
 $factory->define(Student::class, function (Faker $faker) {
     return [
         'idnumber' => $faker->randomNumber($nbDigits = 8),
-        'genre_id' => '1',
         'address' => $faker->streetAddress,
         'birthdate' => $faker->dateTimeThisCentury->format('Y-m-d'),
         'id' => factory(User::class)->create()->id,
