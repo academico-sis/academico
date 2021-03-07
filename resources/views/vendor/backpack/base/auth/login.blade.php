d9cbff50af33f05d732ad159719ae5df05a04eea
Thomas Debay <thomas@debay.org>
2021-03-07 17:44:53 +0100

    Override login page in demo branch

---
+++ resources/views/vendor/backpack/base/auth/login.blade.php
@@ -0,0 +1,66 @@
+@extends(backpack_view('layouts.plain'))
+
+@section('content')
+    <div class="row justify-content-center">
+        <div class="col-12 col-md-8 col-lg-4">
+            <h3 class="text-center mb-4">{{ trans('backpack::base.login') }}</h3>
+            <div class="card">
+                <div class="card-body">
+                    <div class="alert alert-info">Merci de votre intérêt pour le logiciel Academico SIS. Cette version de démonstration est à votre disposition pour tester le logiciel. Elle peut comporter certains bugs ou incohérences qui ne sont pas présents dans les versions de production. Pour plus d'informations sur les offres clé-en-main, consultez <a class="text-white" href="https://www.thomasdebay.com">https://www.thomasdebay.com</a></div>
+                    <form class="col-md-12 p-t-10" role="form" method="POST" action="{{ route('backpack.auth.login') }}">
+                        {!! csrf_field() !!}
+
+                        <div class="form-group">
+                            <label class="control-label" for="{{ $username }}">{{ config('backpack.base.authentication_column_name') }}</label>
+
+                            <div>
+                                <input value="admin@academico.site" type="text" class="form-control{{ $errors->has($username) ? ' is-invalid' : '' }}" name="{{ $username }}" value="{{ old($username) }}" id="{{ $username }}">
+
+                                @if ($errors->has($username))
+                                    <span class="invalid-feedback">
+                                        <strong>{{ $errors->first($username) }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label class="control-label" for="password">{{ trans('backpack::base.password') }}</label>
+
+                            <div>
+                                <input value="secret" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" id="password">
+
+                                @if ($errors->has('password'))
+                                    <span class="invalid-feedback">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div>
+                                <div class="checkbox">
+                                    <label>
+                                        <input type="checkbox" name="remember"> {{ trans('backpack::base.remember_me') }}
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div>
+                                <button type="submit" class="btn btn-block btn-primary">
+                                    {{ trans('backpack::base.login') }}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            @if (config('backpack.base.registration_open'))
+                <div class="text-center"><a href="{{ route('backpack.auth.register') }}">{{ trans('backpack::base.register') }}</a></div>
+            @endif
+        </div>
+    </div>
+@endsection
