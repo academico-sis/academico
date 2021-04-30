38f3c6c54608b8dfd788cc416a36d1c93573a7b0
Thomas Debay <thomas@debay.org>
2021-04-30 09:06:34 +0200

    Install enlightn

--- config/enlightn.php
+++ config/enlightn.php
@@ -15,7 +15,9 @@ return [
     'analyzers' => ['*'],
 
     // If you wish to skip running some analyzers, list the classes in the array below.
-    'exclude_analyzers' => [],
+    'exclude_analyzers' => [
+        Enlightn\Enlightn\Analyzers\Security\LicenseAnalyzer::class,
+    ],
 
     // If you wish to skip running some analyzers in CI mode, list the classes below.
     'ci_mode_exclude_analyzers' => [],
