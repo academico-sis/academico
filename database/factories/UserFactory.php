e19863a1f17a2a16a5b0aba7f8af97df321cfa05
Dummdevka <87282400+Dummdevka@users.noreply.github.com>
2021-12-12 10:58:50 +0300

    Auth as admin

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -7,6 +7,7 @@ $factory->define(App\Models\User::class, function (Faker $faker) {
     return [
         'firstname' => $faker->firstName,
         'lastname' => $faker->lastName,
+        'username' => 'admin@academico.site',
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
