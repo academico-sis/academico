5d19175fd4778be4adc3ea244b20f4f7bbe033a4
Thomas Debay <thomas@debay.org>
2021-12-19 16:16:04 +0100

    Add missing migration

---
+++ database/migrations/2021_12_19_151840_add_send_report_on_column_to_partners_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddSendReportOnColumnToPartnersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (Schema::hasTable('partners') && ! Schema::hasColumn('partners', 'send_report_on')) {
+            Schema::table('partners', function (Blueprint $table) {
+                $table->integer('send_report_on')->nullable();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('partners', function (Blueprint $table) {
+            //
+        });
+    }
+}
