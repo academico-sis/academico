dc389c93ced869077ad2eb22dad5d749160b40a8
Thomas Debay <thomas@debay.org>
2021-12-30 21:24:16 +0100

    Allow to specify student gender

--- config/app.php
+++ config/app.php
@@ -272,8 +272,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Genderize' => Pixelpeter\Genderize\Facades\Genderize::class,
-
     ],
 
 ];
