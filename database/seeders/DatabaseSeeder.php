c91518c3371912008384a9085dc91c125da2efc1
Thomas Debay <thomas@debay.org>
2022-01-06 21:54:14 +0100

    Fix seeder to allow username login

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -297,7 +297,7 @@ class DatabaseSeeder extends Seeder
         $role->givePermissionTo('leads.manage');
 
         $admin = factory(User::class)->create([
-            'email' => 'admin@academico.site',
+            'username' => 'admin',
             'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         ]);
 
