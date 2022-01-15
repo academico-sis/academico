3f1ddc9db2d617e8175175cc7ae8ea9e9d1f9d55
Thomas Debay <thomas@debay.org>
2022-01-15 12:59:50 +0100

    fix tests

--- database/migrations/2022_01_09_180850_add_id_to_skill_evaluations.php
+++ database/migrations/2022_01_09_180850_add_id_to_skill_evaluations.php
@@ -2,15 +2,18 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class AddIdToSkillEvaluations extends Migration
 {
     public function up()
     {
-        Schema::table('skill_evaluations', function (Blueprint $table) {
-            $table->increments('id');
-        });
+        if (DB::connection()->getPDO()->getAttribute(PDO::ATTR_DRIVER_NAME) !== 'sqlite') {
+            Schema::table('skill_evaluations', function (Blueprint $table) {
+                $table->increments('id');
+            });
+        }
     }
 
     public function down()
--- tests/Feature/InvoicingTest.php
+++ tests/Feature/InvoicingTest.php
@@ -17,37 +17,46 @@ class InvoicingTest extends TestCase
 
     public function test_scheduled_payments_can_be_generated_for_enrollment()
     {
+        $this->markTestIncomplete('Test unfinished');
     }
 
     public function test_invoice_can_be_generated_for_product()
     {
+        $this->markTestIncomplete('Test unfinished');
     }
 
     public function test_invoice_can_be_generated_for_enrollment()
     {
+        $this->markTestIncomplete('Test unfinished');
     }
 
     public function test_invoice_can_be_generated_for_scheduled_payment()
     {
+        $this->markTestIncomplete('Test unfinished');
     }
 
     public function test_when_invoice_is_paid_enrollment_is_marked_as_paid()
     {
+        $this->markTestIncomplete('Test unfinished');
     }
 
     public function when_invoice_is_paid_scheduled_payment_is_marked_as_paid()
     {
+        $this->markTestIncomplete('Test unfinished');
     }
 
     public function test_invoice_balance_is_available()
     {
+        $this->markTestIncomplete('Test unfinished');
     }
 
     public function test_invoice_total_price_is_available()
     {
+        $this->markTestIncomplete('Test unfinished');
     }
 
     public function test_invoice_total_paid_price_is_available()
     {
+        $this->markTestIncomplete('Test unfinished');
     }
 }
