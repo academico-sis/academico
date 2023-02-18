e14d16d78fea3e4365e7cfe610e8b397a575cebd
Thomas Debay <thomas@debay.org>
2023-02-18 11:19:40 +0100

    Change fee price field to number

--- app/Http/Controllers/Admin/FeeCrudController.php
+++ app/Http/Controllers/Admin/FeeCrudController.php
@@ -18,6 +18,19 @@ class FeeCrudController extends CrudController
     use UpdateOperation;
     use DeleteOperation;
 
+    private array $currency = [];
+
+    public function __construct()
+    {
+        if (config('academico.currency_position') === 'before') {
+            $this->currency = ['prefix' => config('academico.currency_symbol')];
+        } else {
+            $this->currency = ['suffix' => config('academico.currency_symbol')];
+        }
+
+        parent::__construct();
+    }
+
     public function setup()
     {
         CRUD::setModel(Fee::class);
@@ -67,11 +80,12 @@ class FeeCrudController extends CrudController
                 'label' => __('Name'),
                 'type' => 'text',
             ],
-            [
+            array_merge([
                 'name' => 'price',
                 'label' => __('Price'),
-                'type' => 'text',
-            ],
+                'type' => 'number',
+                'attributes' => ['step' => '0.01'],
+            ], $this->currency),
             [
                 'name' => 'product_code',
                 'label' => __('Product Code'),
