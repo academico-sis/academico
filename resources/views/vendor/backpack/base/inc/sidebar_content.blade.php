22267ffa12eff3e92e85d4da0cafa3edf60dd7c5
Thomas Debay <thomas@debay.org>
2023-02-18 14:51:06 +0100

    Remove legacy shortcut in sidebar

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -108,7 +108,6 @@
                         <li class='nav-item'><a class='nav-link' href='{{ backpack_url('level') }}'><i class='la la-star'></i> <span>@lang('levels')</span></a></li>
                         <li class='nav-item'><a class='nav-link' href='{{ backpack_url('room') }}'><i class='la la-star'></i> <span>@lang('rooms')</span></a></li>
                         <li class='nav-item'><a class='nav-link' href='{{ backpack_url('rhythm') }}'><i class='la la-star'></i> <span>@lang('rhythms')</span></a></li>
-                        <li class='nav-item'><a class='nav-link' href='{{ backpack_url('schedulepreset') }}'><i class='nav-icon la la-question'></i> Schedule Presets</a></li>
                         <li class='nav-item'><a class='nav-link' href='{{ backpack_url('year') }}'><i class='la la-star'></i> <span>@lang('Years')</span></a></li>
                         <li class='nav-item'><a class='nav-link' href='{{ backpack_url('book') }}'><i class='la la-book'></i> <span>@lang('Books')</span></a></li>
                     </ul>
